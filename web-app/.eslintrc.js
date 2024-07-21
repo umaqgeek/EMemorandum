fbc44edf8c5e6d51af87076b935619f8b1c892d9
Umar Mukhtar <umaqgeek@gmail.com>
2024-07-21 11:12:46 +0800

    chore: update base linter

--- web-app/.eslintrc.js
+++ web-app/.eslintrc.js
@@ -1,18 +1,34 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    parser: "@babel/eslint-parser",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "plugin:prettier/recommended",
+    ],
+    parserOptions: {
+        parser: "@babel/eslint-parser",
+    },
+    ignorePatterns: [
+        "dist/", // Ignore built files
+        "build/", // Ignore built files
+        "src/assets/js/**", // Ignore JS files
+    ],
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "prettier/prettier": [
+            "error",
+            {
+                // Your custom Prettier options
+                bracketSpacing: true,
+                printWidth: 80,
+                tabWidth: 4,
+            },
+        ],
+        indent: ["error", 4],
+        // Add or modify other rules as needed
+    },
 };
