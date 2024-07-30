fc6f9ce2625487aa1bf2885dd8ab49f73f907229
Umar Mukhtar <umaqgeek@gmail.com>
2024-07-30 14:05:46 +0800

    feat(migration): add User model and table from migration script

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -11,20 +11,4 @@ public class ApplicationDbContext : DbContext
 
     // Define your DbSets (tables)
     public DbSet<User> Users { get; set; }
-    public DbSet<Product> Products { get; set; }
-}
-
-public class User
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Address { get; set; }
-}
-
-public class Product
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int Quantity { get; set; }
 }
---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+namespace EMemorandum.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Token { get; set; }
+}
