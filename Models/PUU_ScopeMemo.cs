f0767006ed948131920a727ddbd989c0a882b39a
Umar Mukhtar <umaqgeek@gmail.com>
2024-08-06 01:47:08 +0800

    feat(api): add new puu scope memo api

--- Models/PUU_ScopeMemo.cs
+++ Models/PUU_ScopeMemo.cs
@@ -3,6 +3,6 @@ namespace EMemorandum.Models;
 public class PUU_ScopeMemo
 {
     public int Id { get; set; }
-    public string Kod { get; set; }
-    public string Butiran { get; set; }
+    public string? Kod { get; set; }
+    public string? Butiran { get; set; }
 }
