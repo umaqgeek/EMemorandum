8170dfc00c0f7a7b8613f1b23d508d35d7986459
Umar Mukhtar <umaqgeek@gmail.com>
2024-08-30 00:39:06 +0800

    feat(api): ada crud api for puu jenis memo

--- Models/PUU_JenisMemo.cs
+++ Models/PUU_JenisMemo.cs
@@ -5,12 +5,12 @@ namespace EMemorandum.Models;
 public class PUU_JenisMemo
 {
     [Key]
-    public long ID { get; set; }
-    public int Kod { get; set; }
-    public string Butiran { get; set; }
+    public long? ID { get; set; }
+    public int? Kod { get; set; }
+    public string? Butiran { get; set; }
     public string? KodPejabat { get; set; }
     public string? Pejabat { get; set; }
 
     // Navigation property for the memorandums
-    public ICollection<MOU01_Memorandum> Memorandums { get; set; }
+    public ICollection<MOU01_Memorandum>? Memorandums { get; set; }
 }
