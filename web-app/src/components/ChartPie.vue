8d6eefbe8ee113fd602bfc8cbe184d73cce898d4
Umar Mukhtar <umaqgeek@gmail.com>
2024-10-29 11:14:31 +0800

    feat: add pie chart component for frontend side

---
+++ web-app/src/components/ChartPie.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="chart">
+        <apexchart
+            type="pie"
+            width="380"
+            :options="chartOptions"
+            :series="series"
+        ></apexchart>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ChartPieComponent",
+    props: {
+        series: Array,
+        labels: Array,
+    },
+    data(props) {
+        return {
+            chartOptions: {
+                chart: {
+                    width: 380,
+                    type: "pie",
+                },
+                labels: props.labels,
+                responsive: [
+                    {
+                        breakpoint: 480,
+                        options: {
+                            chart: {
+                                width: 200,
+                            },
+                            legend: {
+                                position: "bottom",
+                            },
+                        },
+                    },
+                ],
+            },
+        };
+    },
+};
+</script>
